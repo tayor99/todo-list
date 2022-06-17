66ecce2c4deae4bc05f98d2ce34c66d6411053e3
Adenusi Adetayo <adenusiadetayo@gmail.com>
2022-06-17 10:29:57 +0100

    consumed api

--- src/components/TodoBody.js
+++ src/components/TodoBody.js
@@ -1,29 +1,40 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { AiOutlineDelete } from 'react-icons/ai';
 const TodoBody = () => {
   const [posts, setPost] = useState([]);
   useEffect(() => {
     const getPost = async () => {
       const { data } = await axios.get(
-        "https://robottodoapi.herokuapp.com/api/v1/todo/"
+        'https://robottodoapi.herokuapp.com/api/v1/todo/'
       );
       setPost(data);
     };
     getPost();
-  });
+  }, []);
+
+  const handleDelete = async (id) => {
+    await axios.delete(`https://robottodoapi.herokuapp.com/api/v1/todo/${id}/`);
+  };
 
   return (
-    <>
+    <div className="body-container">
       {posts.map((post) => {
         return (
-          <div className="body-container">
-            <h4 className="title">{post.title}</h4>
-            <p className="description">{post.decriptions}</p>
-          </div>
+          <React.Fragment key={post.id}>
+            <div className="todo-details">
+              <div className="todo-list">
+                <h4 className="title">{post.title}</h4>
+                <p className="description">{post.decriptions}</p>
+              </div>
+              <div className="todo-delete">
+                <AiOutlineDelete onClick={() => handleDelete(post.id)} />
+              </div>
+            </div>
+          </React.Fragment>
         );
       })}
-    </>
+    </div>
   );
 };
 
